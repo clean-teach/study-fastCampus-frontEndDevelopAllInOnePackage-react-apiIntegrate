f4ae300dc9da65ef48e773728b5e89193fcbf55e
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-10-08 03:51:25 +0900

    1. API 연동의 기본 및 CRA 기본 설정 파일 삭제
    ### `useEffect`
    
    - 컴포넌트가 렌더링되는 시점에 요청을 시작
    - `async` / `await` 문과 `try {} catch (error) {}` 문을 사용해서 비동기 작업 구현
        - 참고로, `useEffect` 에 첫번째 파라미터로 등록하는 함수에는 `async` 를 사용 할 수 없기 때문에 함수 내부에서 `async` 를 사용하는 새로운 함수를 선언해주어야 한다.
        - **요청이 시작** 할 때
            - error 와 users 를 초기화하고
            - loading 상태를 true 로 설정
            - `**axios**.get('URL')` 을 사용해서 데이터 불러오기
                - 불러온 데이터는 response에 담는다.
                - users 정보는 `response.data` 에 있다.
        - **에러 발생** 시
            - `try {} catch (error) {}` 문에서 발견한 error를 `setError()` 의 파라미터에 넣어준다.
        - **요청이 완료** 되었을 때
            - loading 상태를 false 로 설정
    
    ### Rendering
    
    - 요청 결과에 따라서 조건문을 사용하여 다른 화면을 랜더링
        - loading 중
        - error 발생
        - 요청 성공
            - 결과 데이터가 없음
            - 데이터 랜더링
                - `Array.map()` 메서드를 사용해서 배열을 순환하며 `JSX` 랜더링
    
    ### 에러 발생 확인하기
    
    - API의 URL을 잘못 입력하여 테스트 해본다.
    
    ## 버튼 눌러서 API 재요청하기
    
    - 버튼을 눌러서 API를 재요청하는 기능을 구현
    1. `fetchUsers` 함수를 바깥으로 분리
    2. Button을 만들어서 event 연결

--- src/Users.js
+++ src/Users.js
@@ -1,15 +1,45 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 function Users() {
     const [users, setUsers] = useState(null);
-    const [loding, setLoding] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    useEffect(() => {}, []);
+    const fetchUsers = async () => {
+        try {
+            setUsers(null);
+            setError(null);
+            setLoading(true);
+
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            // const response = await axios.get('https://jsonplaceholder.typicode.com/users/showmeerror'); // 에러발생 확인용
+
+            setUsers(response.data);
+        } catch(error) {
+            setError(error);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    if(loading) return <div>로딩중...</div>
+    if(error) return <div>에러 발생!</div>
+    if(!users) return <div>검색 결과가 없습니다.</div>
 
     return (
         <>
-            유저 확인  
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.username} ({user.name})
+                    </li>
+                ))}
+            </ul>
+            <button onClick={fetchUsers}>다시 불러오기</button>
         </>
     );
 }
