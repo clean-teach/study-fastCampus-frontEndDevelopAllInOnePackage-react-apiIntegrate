ee5d87a0060890c809374ab0c39eec4b3d64cc15
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-10-08 04:14:56 +0900

    02. useReducer 로 요청 상태 관리하기
    - 이번에는 이전에 구현했던 User 컴포넌트에서 `useState` 대신에 `useReducer` 를 사용해서 구현 해본다.
    - `LOADING`, `SUCCESS`, `ERROR` 액션에 따라 다르게 처리
    - `const {data: users, loading, error} = state;`
        - `state` 를 구조분해 한다.
        - 이때, `state.data` 를 `users` 키워드로 조회 할 수 있다.
    
    ### `useReducer` 로 구현했을 때의 장점
    
    - `useState` 의 `setState` 함수를 여러번 사용하지 않아도 된다.
    - 리듀서로 **로직을 분리**했으니 다른곳에서도 쉽게 재사용을 할 수 있다.

--- src/Users.js
+++ src/Users.js
@@ -1,30 +1,56 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                data: null,
+                loading : true,
+                error: null,
+            }
+        case 'SUCCESS':
+            return {
+                data: action.data,
+                loading : false,
+                error: null,
+            }
+        case 'ERROR':
+            return {
+                data: null,
+                loading : false,
+                error: action.error,
+            }
+        default:
+            throw new Error('Unhandled action type: ' + action.type);
+    }
+}
 
 function Users() {
-    const [users, setUsers] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [state, dispatch] = useReducer(reducer, {
+        data: null,
+        loading : false,
+        error: null,
+    });
 
     const fetchUsers = async () => {
         try {
-            setUsers(null);
-            setError(null);
-            setLoading(true);
+            dispatch({type: 'LOADING'});
 
             const response = await axios.get('https://jsonplaceholder.typicode.com/users');
             // const response = await axios.get('https://jsonplaceholder.typicode.com/users/showmeerror'); // 에러발생 확인용
 
-            setUsers(response.data);
+            dispatch({type: 'SUCCESS', data: response.data});
         } catch(error) {
-            setError(error);
+            dispatch({type: 'ERROR', error});
         }
-        setLoading(false);
     };
 
     useEffect(() => {
         fetchUsers();
     }, []);
+
+    const {data: users, loading, error} = state;
 
     if(loading) return <div>로딩중...</div>
     if(error) return <div>에러 발생!</div>
