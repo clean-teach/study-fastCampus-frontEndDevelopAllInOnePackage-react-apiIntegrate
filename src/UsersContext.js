a28a405b7f7dbcd37ba6c4a3a45585271670d54d
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-10-08 22:51:12 +0900

    헷갈리는 네이밍 수정
    코드가 직관적으로 이해가 되지 않는다면, 이름이 문제일 수 있다.

--- src/UsersContext.js
+++ src/UsersContext.js
@@ -2,19 +2,19 @@ import React, { createContext, useContext, useReducer } from 'react';
 import {initialState, createReducerHandler, createActionDispatcher} from './utils/asyncActionUtils';
 import * as api from './utils/api';
 
-const usersHandler = createReducerHandler('GET_USERS', 'users');
-const userHandler = createReducerHandler('GET_USER', 'user');
+const usersReducer = createReducerHandler('GET_USERS', 'users');
+const userReducer = createReducerHandler('GET_USER', 'user');
 
 function reducer(state, action) {
     switch (action.type) {
         case 'GET_USERS': 
         case 'GET_USERS_SUCCESS': 
         case 'GET_USERS_ERROR': 
-            return usersHandler(state, action);
+            return usersReducer(state, action);
         case 'GET_USER': 
         case 'GET_USER_SUCCESS': 
         case 'GET_USER_ERROR': 
-            return userHandler(state, action);
+            return userReducer(state, action);
         default:
             throw new Error(`Unhandled action type ${action.type}`);
     }
